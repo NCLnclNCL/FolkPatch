35eb13d6ce7572375bf291779886882a76adc052
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-04-27 19:53:28 +0700

    Update supercall.rs

--- apd/src/supercall.rs
+++ apd/src/supercall.rs
@@ -339,7 +339,7 @@ pub fn refresh_ap_package_list(skey: &CStr, mutex: &Arc<Mutex<()>>) {
 pub fn privilege_apd_profile(superkey: &Option<String>) {
     let key = convert_superkey(superkey);
 
-    let all_allow_ctx = "u:r:magisk:s0";
+    let all_allow_ctx = "u:r:hex:s0";
     let profile = SuProfile {
         uid: process::id().try_into().expect("PID conversion failed"),
         to_uid: 0,
