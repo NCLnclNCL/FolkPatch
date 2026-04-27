78d8b4abcb9cf8dca8b28048ce76c358eafa5381
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-04-27 20:26:44 +0700

    Update APatchApp.kt

--- app/src/main/java/me/bmax/apatch/APatchApp.kt
+++ app/src/main/java/me/bmax/apatch/APatchApp.kt
@@ -116,7 +116,7 @@ class APApplication : Application(), Thread.UncaughtExceptionHandler, ImageLoade
         private const val RESETPROP_BIN_PATH = APATCH_BIN_FOLDER + "resetprop"
         private const val KPTOOLS_BIN_PATH = APATCH_BIN_FOLDER + "kptools"
         const val DEFAULT_SCONTEXT = "u:r:untrusted_app:s0"
-        const val MAGISK_SCONTEXT = "u:r:su:s0"
+        const val MAGISK_SCONTEXT = "u:r:hex:s0"
 
         private const val DEFAULT_SU_PATH = "/system/bin/kp"
         private const val LEGACY_SU_PATH = "/system/bin/su"
